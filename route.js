94c5bd9966a6cd36f6dc4784bcf12988680e2f95
Ayomide-Segun <treasure.segun@gmail.com>
2026-04-23 17:21:57 +0100

    fixed

--- route.js
+++ route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { addProfile, getProfileUsingParams, getProfileUsingQuery, naturalLanguageQuery, deleteProfiles  } = require("./controller");
 
 router.post('/profiles', addProfile);
-router.get('/profiles/:id', getProfileUsingParams);
 router.get('/profiles', getProfileUsingQuery);
 router.get('/profiles/search', naturalLanguageQuery);
+router.get('/profiles/:id', getProfileUsingParams);
 router.delete('/profiles/:id', deleteProfiles);
 
 module.exports = router;
